91c6dcbdc1e6dbf9dabc7b99cdfa5fc3564c1950
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-13 16:22:07 +0200

    mod

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const appVue = new Vue({
   // Conterrà tutti i dati necessari all'applicazione
   data: {
 	  // varie chiavi necessarie all'app
+      todolist : ['compra pane','compra pane','compra pane','compra pane']
   },
 	
 	// Conterrà una serie di funzioni
